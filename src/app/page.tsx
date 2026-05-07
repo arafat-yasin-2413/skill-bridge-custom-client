00d8cc2f459913e1770885c2643e8a869cb2eecb
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-07 15:10:22 +0600

    initiates shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   return (
     <div>
         <h2>Hello World</h2>
+
+        <Button>Click here</Button>
     </div>
   );
 }
