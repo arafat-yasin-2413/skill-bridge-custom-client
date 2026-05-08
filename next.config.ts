0ad74ba58f45f78b050aa6e3b8ace712d8dfde11
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-08 16:04:24 +0600

    add: adds theme changer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    
 };
 
 export default nextConfig;
+
+
+// =============== Deployment Help - config ================
+/* config options here */
+    // reactCompiler: true,
+
+    // better-auth proxy
+    // async rewrites() {
+    //     return [
+    //         {
+    //             // Explicitly map auth requests
+    //             source: "/api/auth/:path*",
+    //             destination:
+    //                 process.env.NEXT_PUBLIC_BACKEND_URL + "/api/auth/:path*",
+    //         },
+    //         {
+    //             // Explicitly map v1 API requests
+    //             source: "/api/v1/:path*",
+    //             destination:
+    //                 process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1/:path*",
+    //         },
+    //     ];
+    // },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+
 
 export default function Home() {
-  return (
-    <div>
-        <h2>Hello World</h2>
+    return (
+        <div>
+            <h2>Hello World</h2>
 
-        <Button>Click here</Button>
-    </div>
-  );
+            <Button>Click here</Button>
+
+        </div>
+    );
 }
