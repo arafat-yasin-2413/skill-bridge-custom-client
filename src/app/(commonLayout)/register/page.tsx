ac15b4d0c69122b28ca261875ad2ca9fc0f29605
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-11 11:36:20 +0600

    adds react hook form for registering

--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { RegisterForm } from "@/components/register-form";
 import LogoBrand from "@/components/shared/LogoBrand";
-import { SignupForm } from "@/components/signup-form";
 import Link from "next/link";
 
-export default function SignupPage() {
+import { useForm } from "react-hook-form";
+
+type registerFormValues = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+export default function RegisterPage() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<registerFormValues>();
+
+    const onSubmit = async (data: registerFormValues) => {
+        console.log(data);
+
+        // api call here
+    };
+
     return (
         <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
             <div className="flex w-full max-w-sm flex-col gap-6">
@@ -13,7 +33,14 @@ export default function SignupPage() {
                     className="flex items-center gap-2 self-center font-medium">
                     <LogoBrand />
                 </Link>
-                <SignupForm />
+
+                <RegisterForm
+                    register={register}
+                    handleSubmit={handleSubmit}
+                    onSubmit={onSubmit}
+                    errors={errors}
+                    isSubmitting={isSubmitting}
+                />
             </div>
         </div>
     );
---
+++ src/components/register-form.tsx
@@ -0,0 +1,170 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+} from "@/components/ui/field";
+
+import { Input } from "@/components/ui/input";
+
+import {
+    FieldErrors,
+    SubmitHandler,
+    UseFormHandleSubmit,
+    UseFormRegister,
+} from "react-hook-form";
+
+type registerFormValues = {
+    name: string;
+    email: string;
+    password: string;
+};
+
+type registerFormProps = {
+    register: UseFormRegister<registerFormValues>;
+    handleSubmit: UseFormHandleSubmit<registerFormValues>;
+    onSubmit: SubmitHandler<registerFormValues>;
+    errors: FieldErrors<registerFormValues>;
+    isSubmitting: boolean;
+    className?: string;
+};
+
+export function RegisterForm({
+    className,
+    register,
+    handleSubmit,
+    onSubmit,
+    errors,
+    isSubmitting,
+    ...props
+}: registerFormProps) {
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <CardHeader className="text-center">
+                    <CardTitle className="text-xl">
+                        Create your account
+                    </CardTitle>
+
+                    <CardDescription>
+                        Enter your email below to create your account
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <FieldGroup>
+                            {/* NAME */}
+                            <Field>
+                                <FieldLabel htmlFor="name">
+                                    Full Name
+                                </FieldLabel>
+
+                                <Input
+                                    id="name"
+                                    type="text"
+                                    placeholder="John Doe"
+                                    {...register("name", {
+                                        required: "Name is required",
+                                    })}
+                                />
+
+                                {errors.name && (
+                                    <FieldDescription className="text-red-500">
+                                        {errors.name.message}
+                                    </FieldDescription>
+                                )}
+                            </Field>
+
+                            {/* EMAIL */}
+                            <Field>
+                                <FieldLabel htmlFor="email">Email</FieldLabel>
+
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    {...register("email", {
+                                        required: "Email is required",
+                                        pattern: {
+                                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                            message: "Enter a valid email",
+                                        },
+                                    })}
+                                />
+
+                                {errors.email && (
+                                    <FieldDescription className="text-red-500">
+                                        {errors.email.message}
+                                    </FieldDescription>
+                                )}
+                            </Field>
+
+                            {/* PASSWORD */}
+                            <Field>
+                                <FieldLabel htmlFor="password">
+                                    Password
+                                </FieldLabel>
+
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    {...register("password", {
+                                        required: "Password is required",
+
+                                        minLength: {
+                                            value: 8,
+                                            message:
+                                                "Password must be at least 8 characters",
+                                        },
+                                    })}
+                                />
+
+                                {errors.password ? (
+                                    <FieldDescription className="text-red-500">
+                                        {errors.password.message}
+                                    </FieldDescription>
+                                ) : (
+                                    <FieldDescription>
+                                        Must be at least 8 characters long.
+                                    </FieldDescription>
+                                )}
+                            </Field>
+
+                            {/* BUTTON */}
+                            <Field>
+                                <Button type="submit" disabled={isSubmitting}>
+                                    {isSubmitting
+                                        ? "Creating..."
+                                        : "Create Account"}
+                                </Button>
+
+                                <FieldDescription className="text-center">
+                                    Already have an account?{" "}
+                                    <a href="#">Sign in</a>
+                                </FieldDescription>
+                            </Field>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+
+            <FieldDescription className="px-6 text-center">
+                By clicking continue, you agree to our{" "}
+                <a href="#">Terms of Service</a> and{" "}
+                <a href="#">Privacy Policy</a>.
+            </FieldDescription>
+        </div>
+    );
+}
