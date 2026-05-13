535a60f5eaa2dc181547f387e9fb0593bd3803fb
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2026-05-13 11:39:58 +0600

    adds public and private routes

---
+++ src/lib/routes.ts
@@ -0,0 +1,12 @@
+
+export const privateRoutes = [
+    "/dashboard",
+    "/profile",
+];
+
+export const authRoutes = [
+    "/login",
+    "/register",
+];
+
+export const DEFAULT_LOGIN_REDIRECT = "/"
